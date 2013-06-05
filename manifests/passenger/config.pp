0165099b391fe7479605827361bc5160651c84e9
Leon Bogaert <leon@tim-online.nl>
2013-06-05 21:15:38 +0200

    Passenger doesn't work well on Ubuntu 13.04: removing it again

--- manifests/passenger/config.pp
+++
@@ -1,6 +0,0 @@
-class nginx::passenger::config {
-
-	# do package before config
-	Class['nginx::passenger::package'] -> Class['nginx::passenger::config']
-
-}
--- manifests/passenger/package.pp
+++
@@ -1,9 +0,0 @@
-class nginx::passenger::package {
-
-    apt::ppa { 'ppa:brightbox/passenger': }
-
-    package  { $nginx::passenger::packages:
-        ensure  => $nginx::passenger::ensure,
-        require => Apt::Ppa['ppa:brightbox/passenger'],
-    }
-}
